fa1550adec54a6562bf36e814051f9837498de1b
Marlon <marlonmoncayo7474.com>
2025-09-24 11:37:38 -0500

    Logout de la aplicacion #14

--- lib/screens/auth/register_screen.dart
+++ lib/screens/auth/register_screen.dart
@@ -85,7 +85,6 @@ class RegisterScreen extends StatelessWidget {
                         );
 
                         // Aquí más adelante guardaremos el "username" en Firestore
-
                         Navigator.pushReplacementNamed(context, "/home");
                       } catch (e) {
                         ScaffoldMessenger.of(context).showSnackBar(
