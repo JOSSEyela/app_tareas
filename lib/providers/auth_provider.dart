f89259f4ac54c0655d4eb6d235fed646659c1e1a
Milton-Luna <miltonluna.m.16@gmail.com>
2025-09-24 12:59:55 -0500

    Registro de usuarios con nombre de usuario y correo electronico en una clase en firebase

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -9,27 +9,29 @@ class AuthProvider with ChangeNotifier {
   User? get user => _user;
 
   AuthProvider() {
-    //Escucha los cambios en la sesión de Firebase
+    // Escucha los cambios en la sesión de Firebase
     _authService.userChanges.listen((user) {
       _user = user;
       notifyListeners();
     });
   }
 
-  //Registrar usuario
-  Future<void> register(String email, String password) async {
-    await _authService.register(email, password);
+  // Registrar usuario
+  Future<void> register(String email, String password, String username) async {
+    await _authService.register(email, password, username);
   }
 
   //Iniciar sesión
   Future<void> login(String email, String password) async {
     await _authService.login(email, password);
+    // _user se actualiza automáticamente por el listener
   }
 
   //Cerrar sesión
   Future<void> logout() async {
     await _authService.logout();
-    _user = null; 
-    notifyListeners(); 
+    _user = null;
+    notifyListeners();
   }
 }
+
--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,25 +1,36 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   // Stream: escucha si hay un usuario logueado o no
   Stream<User?> get userChanges => _auth.authStateChanges();
 
-  // Registrar usuario
-  Future<User?> register(String email, String password) async {
+  // Registrar usuario + guardar datos en Firestore
+  Future<User?> register(String email, String password, String username) async {
     try {
+      // 1Crea usuario en Firebase Auth
       final credential = await _auth.createUserWithEmailAndPassword(
         email: email,
         password: password,
       );
+
+      // Guarda datos adicionales en Firestore
+      await _firestore.collection('users').doc(credential.user!.uid).set({
+        'username': username,
+        'email': email,
+        'createdAt': FieldValue.serverTimestamp(),
+      });
+
       return credential.user;
     } on FirebaseAuthException catch (e) {
       throw Exception(e.message);
     }
   }
 
-  // Iniciar sesión
+  //Iniciar sesión
   Future<User?> login(String email, String password) async {
     try {
       final credential = await _auth.signInWithEmailAndPassword(
